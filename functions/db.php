8b183390147df79173da4cb31f4f1d0ec4a8e252
Lingga Pangestu <linggap@Linggas-MacBook-Pro.local>
2017-05-29 15:54:32 +0700

    update

--- functions/db.php
+++ functions/db.php
@@ -1,7 +1,7 @@
 <?php
     $host = "localhost";
     $user = "root";
-    $pass = "";
+    $pass = "root";
     $db = "latihan";
 
     $koneksi = mysqli_connect($host, $user, $pass, $db) or die(mysqli_error());
